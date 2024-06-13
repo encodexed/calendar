ad7211604d7764b4444009f3724dad26aeafca07
Robbie Gollan <robbiegollan@gmail.com>
2024-06-13 17:17:11 +1000

    adds auto focus and border for today's date

--- src/components/DayCard/DayCard.tsx
+++ src/components/DayCard/DayCard.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { ICalendarDay } from "../../services/interfaces";
 import CalendarEvent from "../CalendarEvent/CalendarEvent";
 import styles from "./DayCard.module.scss";
@@ -24,11 +25,37 @@ type Props = {
 
 const DayCard = ({ data, index }: Props) => {
 	const date = data.date.getDate();
+	const todayRef = useRef<HTMLDivElement>(null);
 	// const month = monthNames[data.date.getMonth()];
 	// const year = data.date.getFullYear();
 
+	useEffect(() => {
+		// Scrolls to near today's date
+		if (index == 80) todayRef.current?.scrollIntoView();
+	}, [index]);
+
+	// If day is today, highlight
+	if (index == 90) {
+		return (
+			<div className={styles.card_today} ref={todayRef}>
+				<div className={styles.card_header}>{date}</div>
+				<div
+					className={styles.card_events}
+					onClick={() => {
+						console.log("clicked an event");
+					}}
+				>
+					{data.events.map((ev) => {
+						return (
+							<CalendarEvent key={ev.id} time={ev.startDT} title={ev.title} />
+						);
+					})}
+				</div>
+			</div>
+		);
+	}
 	return (
-		<div className={styles.card}>
+		<div className={styles.card} ref={todayRef}>
 			<div className={styles.card_header}>{date}</div>
 			<div
 				className={styles.card_events}
