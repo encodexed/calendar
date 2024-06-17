60cdeb305c951b0f121e9e67653718b64db6c503
Robbie Gollan <robbiegollan@gmail.com>
2024-06-17 11:27:33 +1000

    adds date and add event button to day info, adds calendar content

--- src/components/DayInfo/DayInfo.tsx
+++ src/components/DayInfo/DayInfo.tsx
@@ -8,28 +8,40 @@ import { useContext, useEffect, useState } from "react";
 const DayInfo = () => {
 	const { selectedDay, events } = useContext(CalendarContext);
 	const [eventsToday, setEventsToday] = useState<IEvent[]>([]);
+	const [todayDate, setTodayDate] = useState<string>("");
+
+	useEffect(() => {
+		setTodayDate(getDateOfCalendarIndex(selectedDay).toDateString());
+	}, [selectedDay]);
 
 	useEffect(() => {
 		const relevantEvents: IEvent[] = [];
-		const todayDate = getDateOfCalendarIndex(selectedDay).toDateString();
 		events.forEach((ev) => {
 			if (ev.startDT.toDateString() === todayDate) {
 				relevantEvents.push(ev);
 			}
 		});
 		setEventsToday(relevantEvents);
-	}, [selectedDay, events]);
+	}, [events, todayDate]);
 
 	return (
 		<>
 			<footer className={styles.footer}>
-				{eventsToday.length > 0 &&
-					eventsToday.map((eve) => {
-						return <EventCard key={eve.id} event={eve} />;
-					})}
-				{eventsToday.length == 0 && (
-					<div className={styles.footer_empty}>No events on this day.</div>
-				)}
+				<div className={styles.day_summary}>
+					<h3>{todayDate}</h3>
+					<button className={styles.day_summary_add_event}>Add Event</button>
+				</div>
+				<div className={styles.day_events}>
+					{eventsToday.length > 0 &&
+						eventsToday.map((eve) => {
+							return <EventCard key={eve.id} event={eve} />;
+						})}
+					{eventsToday.length == 0 && (
+						<div className={styles.footer_empty}>
+							<h3>No events on this day.</h3>
+						</div>
+					)}
+				</div>
 			</footer>
 		</>
 	);
